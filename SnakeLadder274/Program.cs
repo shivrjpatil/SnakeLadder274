c9511e434976be7ebd0bc79fa3f39d64309fb9fe
shivrjpatil <shivrajp972@gmail.com>
2023-04-11 19:11:32 +0530

    Added player checking for options

--- SnakeLadder274/Program.cs
+++ SnakeLadder274/Program.cs
@@ -12,11 +12,34 @@ namespace SnakeLadder274
         {
             Console.WriteLine("Welcome to snake and Ladder Simulator ");
             int PlayerPosition = 0;
+            const int NoPlay = 0, Ladder = 1, Snake = 2;
             Console.WriteLine("lets play! \nPlayerPosition=" + PlayerPosition);
 
             Random random = new Random();
             int Dice = random.Next(1, 7);
             Console.WriteLine("after die rolles the number =" + Dice);
+            // PlayerPosition += Dice;
+            //Console.WriteLine("Player Position " + PlayerPosition);
+
+            int check = random.Next(0, 3);
+            Console.WriteLine(check);
+
+            switch (check)
+            {
+                case NoPlay:
+                    PlayerPosition += 0;
+                    Console.WriteLine("Player Position " + PlayerPosition);
+                    break;
+
+                case Ladder:
+                    PlayerPosition += Dice;
+                    Console.WriteLine("Player Position " + PlayerPosition);
+                    break;
+                case Snake:
+                    PlayerPosition -= Dice;
+                    Console.WriteLine("Player Position " + PlayerPosition);
+                    break;
+            }
             Console.ReadLine();
         }
     }
